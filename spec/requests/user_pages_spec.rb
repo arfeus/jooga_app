55fd745ae1b70676ee5503d08d30322a5da235fc
Ari Vuorenmaa <ari.vuorenmaa@gmail.com>
2013-09-09 18:45:54 +0400

    Finish layout and routes

---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe "User pages" do
+
+  let(:base_title) { "Jooga" }
+
+  describe "signup page" do
+    
+    it "should have the content 'Sign up'" do
+      visit signup_path
+      expect(page).to have_content('Sign up')
+    end
+    
+    it "should have the title 'Sign up'" do
+      visit signup_path
+      expect(page).to have_title("#{base_title} | Sign up")
+    end    
+  end
+end
